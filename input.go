93f12bdf2581fb73a09bbec6a966be15d913c28f
frenklinmici12 <frenklinmici12@gmail.com>
2025-07-30 18:44:32 -0500

    test

--- input.go
+++ input.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"ethos/fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello!!!!!!")
+	fmt.Print("Enter some text: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	// Read until a newline character
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	fmt.Println("You entered:", input)
 }
