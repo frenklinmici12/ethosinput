f77c0e413f7ddc3f1eda865c5dbba575368b1699
frenklinmici12 <frenklinmici12@gmail.com>
2025-07-30 18:52:23 -0500

    test

--- input.go
+++ input.go
@@ -9,15 +9,13 @@ import (
 func main() {
     var input kernelTypes.String
 
-    // Read user input from the console
     status := altEthos.Read("/programs/input", &input)
     if status != syscall.StatusOk {
         var errMsg kernelTypes.String
-        errMsg.Value = "Failed to read input"
+        errMsg = "Failed to read input"
         altEthos.Write("/programs/output", &errMsg)
         return
     }
 
-    // Echo input back to the output
     altEthos.Write("/programs/output", &input)
 }
